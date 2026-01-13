052b460bd55ae56c104728c05c3763da236f932c
Luan121314 <luan.staner@gmail.com>
2026-01-12 21:49:11 -0300

    install spash screen

--- App.tsx
+++ App.tsx
@@ -6,15 +6,22 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
+import { useEffect } from 'react';
 import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets
 } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 
 function App() {
   const isDarkMode = true //useColorScheme() === 'dark';
+
+  useEffect(() => {
+    // Esconde a splash screen após o componente ser montado
+    SplashScreen.hide();
+  }, []);
 
   return (
     <SafeAreaProvider>
